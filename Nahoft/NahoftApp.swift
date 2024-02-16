f241aef2e12a0aa2aaa8cc6bc53eb7dd5d051eb9
u4i-sadra <sadra@united4iran.org>
2024-02-16 17:53:13 +0300

    Add Google Analytics

--- Nahoft/NahoftApp.swift
+++ Nahoft/NahoftApp.swift
@@ -7,9 +7,20 @@
 
 import SwiftUI
 import BackgroundTasks
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+    return true
+  }
+}
 
 @main
 struct NahoftApp: App {
+    // register app delegate for Firebase setup
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     
     init() {
         checkForFirstTime()
